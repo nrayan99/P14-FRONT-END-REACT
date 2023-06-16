742e0f10be52b7a2682c34de762153cfdfa3670f
Rayan NAHI <rayan.nahi99@gmail.com>
2023-06-16 12:09:10 +0200

    feat(employee-list): fix columns

--- src/pages/EmployeeList/index.tsx
+++ src/pages/EmployeeList/index.tsx
@@ -2,26 +2,46 @@ import MUIDataTable, { SelectableRows } from "mui-datatables";
 import { useMemo } from "react";
 import { getEmployees } from "../../api/employee";
 function EmployeeList() {
-  const camelCaseToTitleCase = (camelCase: string) => {
-    const result = camelCase.replace(/([A-Z])/g, " $1");
-    const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
-    return finalResult;
-  };
+
   const data = useMemo(() => getEmployees(), []);
-  const columns = useMemo(
-    () =>
-      Object.keys(data[0]).map((x: string) => {
-        return {
-          name: x,
-          label: camelCaseToTitleCase(x),
-          options: {
-            filter: true,
-            sort: true,
-          },
-        };
-      }),
-    [data]
-  );
+  const columns = [
+    {
+      name: "firstName",
+      label: "First Name",
+    },
+    {
+      name: "lastName",
+      label: "Last Name",
+    },
+    {
+      name: "startDate",
+      label: "Start Date",
+    },
+    {
+      name: "department",
+      label: "Department",
+    },
+    {
+      name: "dateOfBirth",
+      label: "Date of Birth",
+    },
+    {
+      name: "street",
+      label: "Street",
+    },
+    {
+      name: "city",
+      label: "City",
+    },
+    {
+      name: "state",
+      label: "State",
+    },
+    {
+      name: "zipCode",
+      label: "Zip Code",
+    },
+  ]
   const options = {
     selectableRows: "none" as SelectableRows,
   };
