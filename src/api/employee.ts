7787c16713cc3a22edff936283d44e1778ad76b8
Rayan NAHI <rayan.nahi99@gmail.com>
2023-07-21 04:07:24 +0200

    add react-modalium

--- src/api/employee.ts
+++ src/api/employee.ts
@@ -1,21 +1,26 @@
 import { Employee, EmployeeData } from "../types/employee.types";
 import { formatDate } from "../utils/dates";
 
-export function createEmployee(employee: Employee) {
-  const employees = JSON.parse(localStorage.getItem("employees") || "[]") || [];
-  const employeeDatas = {
-    firstName: employee.firstName,
-    lastName: employee.lastName,
-    dateOfBirth: formatDate(employee.dateOfBirth as Date),
-    startDate: formatDate(employee.startDate as Date),
-    department: employee.department.value,
-    street: employee.street,
-    city: employee.city,
-    state: employee.state.value,
-    zipCode: employee.zipCode,
-  };
-  employees.push(employeeDatas);
-  localStorage.setItem("employees", JSON.stringify(employees));
+export async function createEmployee(employee: Employee) {
+  try {
+    const employees = JSON.parse(localStorage.getItem("employees") || "[]") || [];
+    const employeeDatas = {
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      dateOfBirth: formatDate(employee.dateOfBirth as Date),
+      startDate: formatDate(employee.startDate as Date),
+      department: employee.department.value,
+      street: employee.street,
+      city: employee.city,
+      state: employee.state.value,
+      zipCode: employee.zipCode,
+    };
+    employees.push(employeeDatas);
+    localStorage.setItem("employees", JSON.stringify(employees));
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
 }
 
 export async function getEmployees(): Promise<EmployeeData[]> {
--- src/pages/CreateEmployee/index.tsx
+++ src/pages/CreateEmployee/index.tsx
@@ -1,17 +1,37 @@
 import CreateEmployeeForm from "../../components/CreateEmployeeForm";
 import { Employee } from "../../types/employee.types";
 import { createEmployee } from "../../api/employee";
+import { Modal } from "react-modalium";
+import { useState } from "react";
 
 function CreateEmployee() {
-
-  function handleSubmit(employee: Employee) {
-    createEmployee(employee);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [error, setError] = useState(false);
+  async function handleSubmit(employee: Employee) {
+    setError(false);
+    try {
+      await createEmployee(employee);
+    } catch (error) {
+      setError(true);
+    }
+    setIsModalOpen(true);
   }
 
   return (
-    <div>
+    <div id="testt">
       <h2 className="text-center font-bold text-4xl mb-10">Create Employee</h2>
-      <CreateEmployeeForm handleSubmit={handleSubmit}/>
+      <CreateEmployeeForm handleSubmit={handleSubmit} />
+      <Modal isOpen={isModalOpen} close={() => setIsModalOpen(false)}>
+        {error ? (
+          <div className="text-center">
+            <p className="text-xl">Something went wrong</p>
+          </div>
+        ) : (
+          <div className="text-center">
+            <p className="text-xl">Employee created successfully</p>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
